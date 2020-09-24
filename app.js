5d67e1ad9ce90b1a378535f64f8e0adb0ac467c4
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2020-09-24 22:32:49 +0530

    done by bhuvi added coffee and pastries html&&css

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function TurnOnFeedBack(){
-    document.getElementById("FeedBack").style.display="block";
-    }
+// function TurnOnFeedBack(){
+//     document.getElementById("FeedBack").style.display="block";
+//     }
 
-console.log("Lets Go :)");
+// console.log("Lets Go :)");
+
+// let row_cont = document.querySelectorAll(".row")
+// // console.log(row_cont);
+// for(let row in row_cont){
+//     let image = row_cont[row].childNodes[3].childNodes[1];
+//     let style  = `transform : scale(1.1,1.1)`;
+//     console.log(row_cont[row].childNodes[3].childNodes[1]);
+//     row_cont[row].addEventListener("click",()=>{
+//             image.style = style;
+//     })
+// }
