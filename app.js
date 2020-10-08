5c3fe90b809f259a8a9d93478e7d90ab0484af2a
mani-barathi <manibarathi.s@gmail.com>
2020-10-08 21:25:03 +0530

    top btn done

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const goTopBtn = document.getElementById('go-top-btn');
+const goTopBtn = document.getElementById("go-top-btn");
 const feedback = document.getElementById("FeedBack");
 
 // EventListner
-goTopBtn.addEventListener('click', ()=>{
-    window.scrollTo(0,0);
-})
-feedback.addEventListener('click',(e)=>{
-    feedback.classList.toggle("active");
-}
+goTopBtn.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+feedback.addEventListener("click", (e) => {
+  feedback.classList.toggle("active");
+});
 
-window.addEventListener('scroll',()=>{
-    if (window.scrollY > 350 ) {
-            goTopBtn.style.display='block';
-      }
-    else{
-        goTopBtn.style.display='none'; 
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 350) {
+    goTopBtn.style.display = "block";
+  } else {
+    goTopBtn.style.display = "none";
+  }
+});
