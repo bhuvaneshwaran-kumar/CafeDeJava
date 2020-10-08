8dc95c8ef634428a5f2f06bcc523d86a8e1302ae
mani-barathi <manibarathi.s@gmail.com>
2020-10-08 23:04:40 +0530

    footer and forms fixed

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const goTopBtn = document.getElementById("go-top-btn");
 const feedback = document.getElementById("FeedBack");
 
 // EventListner
-goTopBtn.addEventListener("click", () => {
-  window.scrollTo(0, 0);
-});
 feedback.addEventListener("click", (e) => {
   feedback.classList.toggle("active");
+});
+
+goTopBtn.addEventListener("click", () => {
+  window.scrollTo(0, 0);
 });
 
 window.addEventListener("scroll", () => {
